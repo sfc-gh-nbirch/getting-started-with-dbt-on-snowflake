5e5249944bf2cc4cfe3693abf1c8dc653b3ef7a1
sfc-gh-nbirch <nathan.birch@snowflake.com>
2025-04-28 13:05:30 +0000

    Updates

--- tasty_bytes_dbt_demo/examples/dbt_sandbox.sql
+++ tasty_bytes_dbt_demo/examples/dbt_sandbox.sql
@@ -1,2 +1,23 @@
 SHOW TABLES IN DATABASE tasty_bytes_dbt_db;
 SHOW VIEWS IN DATABASE tasty_bytes_dbt_db;
+
+SHOW DBT PROJECTS LIKE 'tasty%';
+
+
+execute dbt project tasty_bytes_dbt_db.dev.tasty_bytes_dbt_dev args='run --select customer_loyalty_metrics';
+
+
+CREATE OR ALTER TASK tasty_bytes_dbt_db.dev.run_prepped_data_dbt
+  SCHEDULE = '24 hours'
+AS
+    execute dbt project tasty_bytes_dbt_db.dev.tasty_bytes_dbt_dev args='run --select customer_loyalty_metrics';
+
+
+ALTER TASK tasty_bytes_dbt_db.dev.run_prepped_data_dbt RESUME;
+
+execute task tasty_bytes_dbt_db.dev.run_prepped_data_dbt;
+
+
+DESCRIBE DBT PROJECT tasty_bytes_dbt_db.dev.tasty_bytes_dbt_dev;
+
+select * from SNOWFLAKE.TELEMETRY.EVENTS order by timestamp desc;
